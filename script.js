5e801ba81c15af461ba85e72d223591dd5b5ce61
avivashishta29 <avivashishta@gmail.com>
2023-03-02 21:23:26 +0530

    fonts

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// Write your script here
-function checkout() {
-  var options = {
-    key: "rzp_test_IYXTqPidWxmY1W", // Enter the Key ID generated from the Dashboard
-    amount: "29935",
-    name: "Acme Corp",
-    description:
-      "A Wild Sheep Chase is the third novel by Japanese author Haruki Murakami",
-    handler: function (response) {
-      alert(response.razorpay_payment_id);
-    },
-    /**
-     * You can track the modal lifecycle by * adding the below code in your options
-     */
-    modal: {
-      ondismiss: function () {
-        console.log("Checkout form closed");
-      },
-    },
-  };
-  var rzp1 = new Razorpay(options);
-}
+// myProducts.filter((item)=>item.title.includes(search.value))
+
+// myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
