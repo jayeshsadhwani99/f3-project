641a49fa410b395a107dbdda1f9641eda2660887
avivashishta29 <avivashishta@gmail.com>
2023-03-02 22:10:46 +0530

    razorpay

--- razorpay/script.js
+++ razorpay/script.js
@@ -4,17 +4,22 @@
 // Add button code documentation:
 // https://razorpay.com/docs/payments/payment-gateway/web-integration/standard/build-integration#code-to-add-pay-button
 
-var options = {
-  key: "rzp_key", // Enter the Key ID generated from the Dashboard
-  amount: "10000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-  currency: "INR",
-  name: "Test",
-  description: "Test Transaction",
-  order_id: "rzp_order_id", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-  callback_url: "callback_url",
-};
-var rzp1 = new Razorpay(options);
 document.getElementById("rzp-button1").onclick = function (e) {
-  rzp1.open();
+  var options = {
+    key: "rzp_test_wzFXFHKU56LGex", // Enter the Key ID generated from the Dashboard
+    amount: 300 * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    currency: "INR",
+    name: "MyShop Checkout",
+    description: "This is your order", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    theme: {
+      color: "#000",
+    },
+    image:
+      "https://www.mintformations.co.uk/blog/wp-content/uploads/2020/05/shutterstock_583717939.jpg",
+  };
+
+  var rzpy1 = new Razorpay(options);
+  rzpy1.open();
+  // clear mycart
   e.preventDefault();
 };
